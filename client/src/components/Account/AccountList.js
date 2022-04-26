9392219dc9f5dfd7aa3d5f886f8cdae23dd45ef1
DOBBI <cocomong_94@naver.com>
2022-04-26 15:58:13 +0900

    feat:가계부 기능 구현 1차

---
+++ client/src/components/Account/AccountList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Modal from '../common/Modal';
+import AccountEditor from './AccountEditor';
+import AccountWriteUp from './AccountWriteUp';
+
+function AccountList({ AccountList, onEdit, onRemove, onCreate }) {
+  const a = new Date().toLocaleString();
+  return (
+    <div className="AccountList">
+      <div className="AccountListSpanBox">
+        <h4>{AccountList.length}개의 기록이 있어요 !</h4>
+        <Modal>
+          <AccountWriteUp onCreate={onCreate} />
+        </Modal>
+      </div>
+      <div>
+        {AccountList.map(it => (
+          <AccountEditor
+            key={it.id}
+            {...it}
+            onEdit={onEdit}
+            onRemove={onRemove}
+            onCreate={onCreate}
+            AccountList={AccountList}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+AccountList.defaultProps = {
+  AccountList: ['에러발생 !'],
+};
+
+export default AccountList;
