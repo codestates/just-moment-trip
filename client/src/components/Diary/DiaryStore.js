458dc63c1bc53b30e4fe38947dbf45969bf16cf9
manseon <91870110+full-fish@users.noreply.github.com>
2022-05-10 14:41:49 +0900

    fix

--- client/src/components/Diary/DiaryStore.js
+++ client/src/components/Diary/DiaryStore.js
@@ -63,8 +63,8 @@ function DiaryStore() {
 
   function getData() {
     let accessToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJtYW5zZW9uQG5hdmVyLmNvbSIsImlhdCI6MTY1MjA3OTM2MywiZXhwIjoxNjUyMTg3MzYzfQ.xbjdPPuQNiFpNQuVShyQbC302BMuLlMAQJOMu3Vtk40';
-    let url = 'https://www.just-momet-trip.tk/diary?trip_id=1';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE2NTIxNjA5MDQsImV4cCI6MTY1MjI2ODkwNH0.5sQondqGTQ5OdOhfxyEZfL8rZz06cDC6z8Iuxt-6Wlk';
+    let url = 'https://www.just-moment-trip.tk/diary?trip_id=1';
 
     if (search) url += `&search=${search}`;
     axios
@@ -78,7 +78,7 @@ function DiaryStore() {
         if (data.data.accessToken) accessToken = data.data.accessToken;
         const initData = data.data.data;
         dispatch({ type: INIT, data: initData });
-    });
+      });
   }
 
   useEffect(() => {
