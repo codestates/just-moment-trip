2cb9c78863ca0df52434c749f2e638d3e7e8aa95
Hyunmin Choi <chlgusals3@gmail.com>
2022-05-12 01:52:13 +0900

    fix : 8080 -> 9000

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ hashtag_delete_schedule.cron();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://www.just-moment-trip.ml", "http://localhost:8080"],
+    origin: ["https://www.just-moment-trip.ml", "http://localhost:9000"],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   })
