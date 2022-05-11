1b03305c7e12fa1d390caf24f74b2e1da6aaf52e
DOBBI <cocomong_94@naver.com>
2022-05-11 20:55:08 +0900

    CSS : 짜잘짜잘 CSS

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 const NavbarBox = styled.div`
   display: flex;
   position: fixed;
+  justify-content: space-between;
   top: 0;
   left: 0;
   right: 0;
@@ -19,30 +20,40 @@ const NavbarBox = styled.div`
 function Navbar() {
   return (
     <NavbarBox>
-      <div className="Navbar_1" style={{ padding: '0 5px' }}>
-        <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
-          HOME
-        </Link>
+      <div>
+        <div className="Navbar_1" style={{ padding: '0 5px' }}>
+          <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
+            HOME
+          </Link>
+        </div>
       </div>
-      <div className="Navbar_2" style={{ padding: '0 5px' }}>
-        <Link style={{ textDecoration: 'none', color: 'black' }} to="/diary">
-          DIARY
-        </Link>
-      </div>
-      <div className="Navbar_3" style={{ padding: '0 5px' }}>
-        <Link style={{ textDecoration: 'none', color: 'black' }} to="/account">
-          ACCOUNT
-        </Link>
-      </div>{' '}
-      <div className="Navbar_4" style={{ padding: '0 5px' }}>
-        <Link style={{ textDecoration: 'none', color: 'black' }} to="/mypage">
-          MYPAGE
-        </Link>
-      </div>
-      <div className="Navbar_5" style={{ padding: '0 5px' }}>
-        <Link style={{ textDecoration: 'none', color: 'black' }} to="/aboutUs">
-          ABOUTUS
-        </Link>
+      <div style={{ display: 'flex' }}>
+        <div className="Navbar_2" style={{ padding: '0 5px' }}>
+          <Link style={{ textDecoration: 'none', color: 'black' }} to="/diary">
+            DIARY
+          </Link>
+        </div>
+        <div className="Navbar_3" style={{ padding: '0 5px' }}>
+          <Link
+            style={{ textDecoration: 'none', color: 'black' }}
+            to="/account"
+          >
+            ACCOUNT
+          </Link>
+        </div>{' '}
+        <div className="Navbar_4" style={{ padding: '0 5px' }}>
+          <Link style={{ textDecoration: 'none', color: 'black' }} to="/mypage">
+            MYPAGE
+          </Link>
+        </div>
+        <div className="Navbar_5" style={{ padding: '0 5px' }}>
+          <Link
+            style={{ textDecoration: 'none', color: 'black' }}
+            to="/aboutUs"
+          >
+            ABOUTUS
+          </Link>
+        </div>
       </div>
     </NavbarBox>
   );
