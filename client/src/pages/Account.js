baa920fcb02276020cbe73a45beaaaf93a592065
DOBBI <cocomong_94@naver.com>
2022-05-11 11:57:24 +0900

    CSS : 폰트 및 여러 CSS 적용

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import AccountList from '../components/Account/AccountList';
 import AccountStore from '../components/Account/AccountStore';
-import Footer from '../components/common/Footer';
 import Navbar from '../components/common/Navbar';
 
 const Box = styled.div`
   display: flex;
   justify-content: center;
   border: 10px solid #312352;
+  font-family: SBFont;
 `;
 
 function Account() {
