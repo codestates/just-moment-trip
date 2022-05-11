7d23044a0ea9260fcbcd442550b9e8c9cbcd138d
Hyunmin Choi <chlgusals3@gmail.com>
2022-05-11 20:26:38 +0900

    refactor : MyPage Requests

--- client/src/components/MyPage/InfoButton.js
+++ client/src/components/MyPage/InfoButton.js
@@ -26,15 +26,25 @@ function InfoButton() {
     },
   };
 
-  const userPatchHandler = async (email, password, new_password) => {
-    options.data = { email, password, new_password };
-    await axios.patch(url, options);
+  const userPatchHandler = input => {
+    axios
+      .patch(url, input, options)
+      .then(res => {
+        alert('회원정보 변경 완료 다시 로그인 해주세요');
+        navigate('/');
+        //로그인 off
+        //토큰 삭제
+      })
+      .catch(err => {
+        alert('입력정보 다름');
+      });
     delete options.data;
   };
 
   const signoutHandler = async () => {
     await axios.post('https://www.just-moment-trip.tk/sign/out', options);
     //로그인 했는지 안했는지 상태관리 하는게 있다면 false로 바꿔주기
+    //토큰 빈 문자열로 바꿔주기
     navigate('/');
   };
 
@@ -47,6 +57,7 @@ function InfoButton() {
   const userDeleteHandler = async () => {
     await axios.delete(url, options);
     //로그인 했는지 안했는지 상태관리 하는게 있다면 false 로 바꿔주기
+    //토큰 빈 문자열로 바꿔주기
     navigate('/');
   };
 
