6ca0b0379943038aaf304abeda20e55419ec79ea
Hyunmin Choi <chlgusals3@gmail.com>
2022-05-12 12:56:36 +0900

    feat : Info Page added Trip

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -3,11 +3,13 @@ import Navbar from '../components/common/Navbar';
 import AccountInfo from '../components/Info/AccountInfo';
 import DiaryInfo from '../components/Info/DiaryInfo';
 import GraphInfo from '../components/Info/GraphInfo';
+import TripInfo from '../components/Info/TripInfo';
 
 function Info() {
   return (
     <>
       <Navbar />
+      <TripInfo />
       <DiaryInfo />
       <AccountInfo />
       <GraphInfo />
