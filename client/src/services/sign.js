c5cbf5ad5d48fc4891955866703fe005390b71c4
manseon <91870110+full-fish@users.noreply.github.com>
2022-05-29 22:41:57 +0900

    feat:sign up할때 클라이언트와 서버 통신간의 RSA적용

--- client/src/services/sign.js
+++ client/src/services/sign.js
@@ -1,20 +1,42 @@
 import axios from 'axios';
 
 export const signCustomApi = axios.create({
-  // baseURL: 'http://localhost:8080/sign/',
-  baseURL: 'https://www.just-moment-trip.tk/sign/',
+  baseURL: 'http://localhost:8080/sign/',
+  // baseURL: 'https://www.just-moment-trip.tk/sign/',
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
 
-export const signUpApi = (email, nickname, password) => {
-  signCustomApi.post('up', {
+export const signUpApi = async (email, nickname, password) => {
+  const res = await signCustomApi.post('up', {
+    createKey: true,
+    nickname,
+    email,
+  });
+  let encrypted = [];
+  const e = BigInt(Number(JSON.parse(res.data.data.e)));
+  const N = BigInt(Number(JSON.parse(res.data.data.N)));
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+  for (let i = 0; i < password.length; i++) {
+    let a = BigInt(password[i].charCodeAt(0));
+    let total = 1n;
+    // 리액트에서는 **이 자동으로 Math.pow로 변해서 BigInt에서 못 읽어버림 그래서 for문으로 함
+    for (let j = 0n; j < e; j++) {
+      total *= a;
+    }
+    encrypted[i] = JSON.stringify(total % N);
+  }
+
+  const res2 = await signCustomApi.post('up', {
     email,
     nickname,
-    password,
+    password: encrypted,
   });
+  return res2;
 };
 
 export const signInApi = async (email, password) => {
