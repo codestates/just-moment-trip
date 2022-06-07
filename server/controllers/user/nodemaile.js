9d5b34d24dd348e5e9337478871973499353ebc1
manseon <91870110+full-fish@users.noreply.github.com>
2022-06-07 18:22:06 +0900

    feat:메일내에 프로젝트레포로가는 링크 추가

--- server/controllers/user/nodemaile.js
+++ server/controllers/user/nodemaile.js
@@ -14,12 +14,14 @@ exports.sendEmail = (email, newPassword, subject) => {
     })
   );
 
-  var mailOption;
-  mailOption = {
+  var mailOption = {
     from: process.env.NAVER_EMAIL, // 보내는 분의 메일계정
     to: email, // 받는 분의 메일계정 (여러 개 가능)
     subject: subject,
-    text: newPassword,
+    html:
+      `<h1>${subject} : ${newPassword}</h1>` +
+      `<h3><div>저희 프로젝트의 Git 레포입니다</div> <div>아래 이미지를 눌러주세요</div></h3><a href="https://github.com/codestates/just-moment-trip">` +
+      '<img src="https://user-images.githubusercontent.com/89363516/165240860-f200568c-6e65-4c88-ab04-d2789e29c8f1.png" height="250" width="250"/></p></a>',
   };
 
   smtpTransport.sendMail(mailOption, (err, response) => {
