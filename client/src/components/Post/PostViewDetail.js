35ffcde532f864b51151d79175018e659c27c620
DOBBI <cocomong_94@naver.com>
2022-06-25 20:32:19 +0900

    FEAT : POST DELETE

--- client/src/components/Post/PostViewDetail.js
+++ client/src/components/Post/PostViewDetail.js
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ReactHtmlParser from 'react-html-parser'; //태그를 문자열로 바꿔줌 ! 이거 설정해야함 !
-/* ReactHtmlParser(content) */
 import Swal from 'sweetalert2';
+import axios from 'axios';
 import spongebob from '../../Assets/spongebob.gif';
 import CommentList from './Comment/CommentList';
 import {
@@ -14,9 +14,15 @@ import {
   MiddelSentence,
   MiddleBox,
   PostViewDetailBox,
+  Content,
 } from './styles';
 
-function PostViewDetail({ data }) {
+function PostViewDetail() {
+  const [isEdit, setIsEdit] = useState(false);
+  const url = 'http://localhost:8080';
+  const token = JSON.parse(sessionStorage.getItem('user'))?.accessToken;
+  const userNickname = JSON.parse(sessionStorage.getItem('user'))?.data
+    .nickname;
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -43,8 +49,62 @@ function PostViewDetail({ data }) {
     }
   }, []);
 
-   useCallback(() => { },[])
+  const editHandler = useCallback(() => {
+    setIsEdit(true);
+  }, []);
 
+  const onDelete = useCallback(() => {
+    Swal.fire({
+      title: `게시글을 삭제할까요 ?`,
+      icon: 'question',
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonText: '네',
+      cancelButtonText: '아니오',
+      backdrop: `
+        rgba(0,0,110,0.5)
+        url(${spongebob})
+        no-repeat
+      `,
+    }).then(res => {
+      if (res.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: '삭제 완료!',
+          confirmButtonText: '알겠어요',
+          backdrop: `
+            rgba(0,0,110,0.5)
+            url(${spongebob})
+            left bottom
+            no-repeat
+          `,
+        });
+        axios
+          .delete(
+            `${url}/post/${location.state?.data.id}`,
+            {
+              id: location.state?.data.id,
+            },
+            {
+              headers: {
+                authorization:
+                  'Bearer ' +
+                  JSON.parse(sessionStorage.getItem('user'))?.accessToken,
+                'Content-Type': 'application/json',
+              },
+              withCredentials: true,
+            },
+          )
+          .then(() => navigate('/post'));
+      }
+    });
+  }, []);
+  console.log('id확인', location.state?.data.id);
+  const onEdit = useCallback(() => {
+    console.log('수정버튼활성화');
+  }, []);
+
+  console.log('------PostViewDetail', location.state?.data.nickname);
   return (
     <PostViewDetailBox>
       <Header>글제목 : {location.state?.data.title}</Header>
@@ -57,11 +117,24 @@ function PostViewDetail({ data }) {
         </MiddelSentence>
       </MiddleBox>
       <ContentBox>
-        <p>{location.state?.data.content}</p>
+        <Content>{ReactHtmlParser(location.state?.data.content)}</Content>
       </ContentBox>
       <BtnBox>
-        <Btn>수정하기</Btn>
-        <Btn>삭제하기</Btn>
+        {token &&
+          // eslint-disable-next-line no-nested-ternary
+          (userNickname === location.state?.data.nickname ? (
+            isEdit ? (
+              <>
+                <Btn onClick={() => {}}>수정완료</Btn>
+                <Btn onClick={() => setIsEdit(false)}>수정취소</Btn>
+              </>
+            ) : (
+              <>
+                <Btn onClick={editHandler}>수정하기</Btn>
+                <Btn onClick={onDelete}>삭제하기</Btn>
+              </>
+            )
+          ) : null)}
       </BtnBox>
       <ContentListBox>
         <CommentList />
